889c5c0dffff6488cc918edb49b0482cd5e52bd5
swethashree2005 <swethasundaram2005@gmail.com>
2025-10-31 22:38:30 +0530

    Fix: removed any type from Dashboard

--- apps/web/lib/getUserSession.ts
+++ apps/web/lib/getUserSession.ts
@@ -1,9 +1,10 @@
 // lib/getUserSession.ts
 import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/ssr'
+import { createServerClient } from "@supabase/ssr";
+
 
 export async function getUserSession() {
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = createServerClient({ cookies });
   const { data: { session } } = await supabase.auth.getSession();
   return { user: session?.user || null };
 }
